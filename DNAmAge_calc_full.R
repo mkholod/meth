819446e04ab9d32ee31c3dbb44ad53324f07b86a
Michael Kholodenko <michaelk@ownbackup.com>
2021-11-14 21:48:58 +0200

    extend to meta

--- DNAmAge_calc_full.R
+++ DNAmAge_calc_full.R
@@ -51,3 +51,29 @@ library(GEOquery)
 # myData=read.csv.sql("beta1.csv")
 myDNAmAge <- DNAmAge(beta)
 write.csv(myDNAmAge, "DNAmAge_full.csv")
+
+# read real age
+# meta <- read.csv("../../RAW/Sample_Sheet_Full.csv", header=TRUE)
+dataOnly = read.csv("../../RAW/Sample_Sheet_Full.csv", skip = 8, header = F)
+headers = read.csv("../../RAW/Sample_Sheet_Full.csv", skip = 7, header = F, nrows = 1, as.is = T)
+colnames(dataOnly) = headers
+metadata_file <- dataOnly
+
+#meta_with_data_only <- meta[-c(1:6),]
+#real_age <- meta$X.4[8:103]
+
+# add column to myDNAmAge
+#myDNAmAge_with_real_age <- myDNAmAge
+#myDNAmAge_with_real_age$real_age <- real_age
+#write.csv(myDNAmAge_with_real_age, "DNAmAge_full_with_real_age.csv")
+
+# remove PedBE, Wu, TL
+#write.csv(myDNAmAge_with_real_age[,c("id", "Horvath", "Hannum", "Levine", "skinHorvath", "real_age")], "DNAmAge_full_with_real_age_without_pedbe_wu_tl.csv")
+
+# add metadata
+myDNAmAge_with_metadata <- myDNAmAge
+myDNAmAge_with_metadata <- cbind(myDNAmAge_with_metadata, metadata_file)
+
+drop_cols <- c("PedBE", "Wu", "TL")
+myDNAmAge_with_metadata_without_pedbe_wu_tl <- myDNAmAge_with_metadata[ , !(names(myDNAmAge_with_metadata) %in% drop_cols)]
+write.csv(myDNAmAge_with_metadata_without_pedbe_wu_tl, "myDNAmAge_with_metadata_without_pedbe_wu_tl.csv")
