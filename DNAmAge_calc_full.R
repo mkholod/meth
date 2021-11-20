77472fa421b0b04d8b01cabdf74eb7c0420aafeb
Michael Kholodenko <michaelk@ownbackup.com>
2021-11-20 10:59:44 +0200

    Adding acceleration data for samples

--- DNAmAge_calc_full.R
+++ DNAmAge_calc_full.R
@@ -16,7 +16,13 @@ library(minfi)
 library(minfiData)
 library(sva)
 
-baseDir <- "../../RAW"
+is_full_data <- FALSE # full 96 samples if TRUE, 16 samples for speed if FALSE
+if (is_full_data) {
+  baseDir <- "RAW"
+} else {
+  baseDir <- "samples"
+}
+
 targets <- read.metharray.sheet(baseDir)
 RGSet <- read.metharray.exp(targets = targets)
 phenoData <- pData(RGSet)
@@ -41,15 +47,31 @@ library(GEOquery)
 myDNAmAge <- DNAmAge(beta)
 
 # read the metadata
-dataOnly = read.csv("../../RAW/Sample_Sheet_Full.csv", skip = 8, header = F)
-headers = read.csv("../../RAW/Sample_Sheet_Full.csv", skip = 7, header = F, nrows = 1, as.is = T)
+
+if (is_full_data) {
+  sample_sheet_filename = "/Sample_Sheet_Full.csv"
+} else {
+  sample_sheet_filename = "/Sample_Sheet_16.csv"
+}
+
+path <- paste(baseDir, sample_sheet_filename, sep="")
+dataOnly = read.csv(path, skip = 8, header = F)
+headers = read.csv(path, skip = 7, header = F, nrows = 1, as.is = T)
+
 colnames(dataOnly) = headers
 metadata_file <- dataOnly
 
+# calculate ageAcceleration as specified in 4.2 in https://www.bioconductor.org/packages/devel/bioc/vignettes/methylclock/inst/doc/methylclock.html
+myDNAmAge_with_acceleration_age <- DNAmAge(beta, age=metadata_file$Age, cell.count=TRUE)
+
 # bind metadata with the DNAmAge
-myDNAmAge_with_metadata <- cbind(myDNAmAge, metadata_file)
+myDNAmAge_with_acceleration_age_with_metadata <- cbind(myDNAmAge_with_acceleration_age, metadata_file)
 
 # remove unneeded columns
 drop_cols <- c("PedBE", "Wu", "TL", "BNN")
-myDNAmAge_with_metadata_without_pedbe_wu_tl_bnn <- myDNAmAge_with_metadata[ , !(names(myDNAmAge_with_metadata) %in% drop_cols)]
-write.csv(myDNAmAge_with_metadata_without_pedbe_wu_tl_bnn, "myDNAmAge_with_metadata_without_pedbe_wu_tl_bnn.csv")
+myDNAmAge_with_acceleration_with_metadata_without_pedbe_wu_tl_bnn <- myDNAmAge_with_acceleration_age_with_metadata[ , !(names(myDNAmAge_with_acceleration_age_with_metadata) %in% drop_cols)]
+write.csv(myDNAmAge_with_acceleration_with_metadata_without_pedbe_wu_tl_bnn, "myDNAmAge_with_acceleration_with_metadata_without_pedbe_wu_tl_bnn.csv")
+
+
+
+
