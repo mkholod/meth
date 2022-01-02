2b6aca78fd2b7839ad79da58313ff5024cf59c45
Michael Kholodenko <michaelk@ownbackup.com>
2022-01-02 15:59:12 +0200

    Adding TTEST #2

--- DNAmAge_calc_full.R
+++ DNAmAge_calc_full.R
@@ -60,14 +60,19 @@ metadata_file <- dataOnly
 
 # calculate ageAcceleration as specified in 4.2 in https://www.bioconductor.org/packages/devel/bioc/vignettes/methylclock/inst/doc/methylclock.html
 myDNAmAge_with_acceleration_age <- DNAmAge(beta, age=metadata_file$Age, cell.count=TRUE)
+cpgs.missing <- checkClocks(myDNAmAge_with_acceleration_age)
+cpgs.missing.GA <- checkClocksGA(myDNAmAge_with_acceleration_age)
 
 # bind metadata with the DNAmAge
 myDNAmAge_with_acceleration_age_with_metadata <- cbind(myDNAmAge_with_acceleration_age, metadata_file)
+
+write.csv(myDNAmAge_with_acceleration_age_with_metadata, "csv/myDNAmAge_with_acceleration_age_with_metadata.csv")
 
 # remove unneeded columns
 drop_cols <- c("PedBE", "Wu", "TL", "BNN")
 myDNAmAge_with_acceleration_with_metadata_without_pedbe_wu_tl_bnn <- myDNAmAge_with_acceleration_age_with_metadata[ , !(names(myDNAmAge_with_acceleration_age_with_metadata) %in% drop_cols)]
 write.csv(myDNAmAge_with_acceleration_with_metadata_without_pedbe_wu_tl_bnn, "myDNAmAge_with_acceleration_with_metadata_without_pedbe_wu_tl_bnn.csv")
+
 
 #plot 
 plotDNAmAge(myDNAmAge_with_acceleration_with_metadata_without_pedbe_wu_tl_bnn$Horvath, metadata_file$Age)
@@ -131,6 +136,82 @@ write.csv(avg_by_clock, out_file_avg)
 
 colnames(sd_by_clock) <- c("clock_type", "Yes_sd", "No_sd")
 write.csv(sd_by_clock, out_file_sd)
+
+# To perform a TTEST for acceleration1 for Hovarth and Levine
+# And check if there is a difference between Sporadic and Vhl
+# using https://www.youtube.com/watch?v=RlhnNbPZC0A&ab_channel=MarinStatsLectures-RProgramming%26Statistics
+all_data = myDNAmAge_with_acceleration_age_with_metadata
+boxplot(myDNAmAge_with_acceleration_age_with_metadata$ageAcc.Levine ~ myDNAmAge_with_acceleration_age_with_metadata$Sample_Group)
+boxplot(myDNAmAge_with_acceleration_age_with_metadata$ageAcc.Horvath ~ myDNAmAge_with_acceleration_age_with_metadata$Sample_Group)
+
+t.test(all_data$ageAcc.Horvath[all_data$Sample_Group == "Sporadic"], all_data$ageAcc.Horvath[all_data$Sample_Group == "VHL"])
+
+#Welch Two Sample t-test
+
+#data:  all_data$ageAcc.Horvath[all_data$Sample_Group == "Sporadic"] and all_data$ageAcc.Horvath[all_data$Sample_Group == "VHL"]
+#t = -0.92099, df = 39.473, p-value = 0.3626
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -13.464380   5.036927
+#sample estimates:
+#  mean of x mean of y 
+#8.956855 13.170581 
+
+t.test(all_data$ageAcc.Levine[all_data$Sample_Group == "Sporadic"], all_data$ageAcc.Levine[all_data$Sample_Group == "VHL"])
+
+# Welch Two Sample t-test
+
+#data:  all_data$ageAcc.Levine[all_data$Sample_Group == "Sporadic"] and all_data$ageAcc.Levine[all_data$Sample_Group == "VHL"]
+#t = -0.95366, df = 38.685, p-value = 0.3462
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -12.430501   4.466156
+#sample estimates:
+#  mean of x mean of y 
+#-15.76965 -11.78748 
+
+t.test(all_data$ageAcc2.Horvath[all_data$Sample_Group == "Sporadic"], all_data$ageAcc2.Horvath[all_data$Sample_Group == "VHL"])
+
+# Welch Two Sample t-test
+
+# data:  all_data$ageAcc2.Horvath[all_data$Sample_Group == "Sporadic"] and all_data$ageAcc2.Horvath[all_data$Sample_Group == "VHL"]
+# t = 0.24433, df = 19.742, p-value = 0.8095
+# alternative hypothesis: true difference in means is not equal to 0
+# 95 percent confidence interval:
+#  -9.04976 11.44871
+#sample estimates:
+#  mean of x  mean of y 
+#0.9136663 -0.2858094 
+
+t.test(all_data$ageAcc2.Levine[all_data$Sample_Group == "Sporadic"], all_data$ageAcc2.Levine[all_data$Sample_Group == "VHL"])
+
+
+#Welch Two Sample t-test
+
+#data:  all_data$ageAcc2.Levine[all_data$Sample_Group == "Sporadic"] and all_data$ageAcc2.Levine[all_data$Sample_Group == "VHL"]
+#t = 0.064188, df = 28.41, p-value = 0.9493
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -8.272391  8.807964
+#sample estimates:
+#  mean of x mean of y 
+#3.897955  3.630168 
+
+t.test(all_data$ageAcc.Horvath[all_data$Sample_Group != "VHL"], all_data$ageAcc.Horvath[all_data$Sample_Group == "VHL"])
+
+# Welch Two Sample t-test
+
+#data:  all_data$ageAcc.Horvath[all_data$Sample_Group != "VHL"] and all_data$ageAcc.Horvath[all_data$Sample_Group == "VHL"]
+#t = -1.2458, df = 21.332, p-value = 0.2263
+#alternative hypothesis: true difference in means is not equal to 0
+#95 percent confidence interval:
+#  -11.822740   2.959044
+#sample estimates:
+#  mean of x mean of y 
+#8.738733 13.170581 
+
+t.test(all_data$ageAcc.Levine[all_data$Sample_Group != "VHL"], all_data$ageAcc.Levine[all_data$Sample_Group == "VHL"])
+
 
 
 # TODO metastasis predicition accel vs features
