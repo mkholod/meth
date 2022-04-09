ff7006a7f39f78e31f7c1e21a79a67843ec81911
Michael Kholodenko <michaelk@ownbackup.com>
2022-04-09 19:51:50 +0300

    metastasis prediction on all the beta

--- knn_predict.r
+++ knn_predict.r
@@ -15,7 +15,7 @@ colnames(dataOnly) = headers
 metadata_file <- dataOnly
 # metadata_file_pnet_only <- metadata_file[metadata_file$Site == 'PNET', ]
 
-metadata_sample_group <- metadata_file$Sample_Group
+metadata_sample_group <- metadata_file$AnyMet
 metadata_sample_group_len <- length(metadata_sample_group)
 # testset contains around 10% of the original set
 train_coef <- 0.9
@@ -23,8 +23,8 @@ train_len <- as.integer(metadata_sample_group_len * train_coef)
 train_target <- as.integer(as.factor(metadata_sample_group[1:train_len]))
 test_target <- as.integer(as.factor(metadata_sample_group[(train_len + 1):metadata_sample_group_len]))
 
-csv_dir <- file.path(getwd(), "csv")
-beta_csv_path <- file.path(csv_dir, "levine_beta.csv") 
+csv_dir <- file.path(getwd(), "")
+beta_csv_path <- file.path(csv_dir, "beta.csv") 
 
 # lines_to_keep <- 100000
 # total_lines <- R.utils::countLines(beta_csv_path) # 866092
@@ -37,8 +37,7 @@ beta_without_na <- na.omit(beta) # remove cgX which have NA values since KNN nee
 # transpose so we have a row of a single patient instead of a column
 data_frame <- data.frame(t(beta_without_na))
 
-# use data_frame only for PNET site
-# data_frame_pnet_only <- data_frame[metadata_file$Site == 'PNET', ]
+# data_frame_pnet_only <- data_frame[metadata_file$Site == 'PNET', ] # use data_frame only for PNET site
 
 # normalize to have distributed values between 0 and 1
 data_frame_norm <- as.data.frame(lapply(data_frame, min_max_norm))
@@ -48,7 +47,7 @@ train_set <- data_frame_norm[1:train_len, ]
 test_set <- data_frame_norm[(train_len + 1):metadata_sample_group_len, ]
 
 require(class)
-classifier_knn <- knn(train=train_set, test=test_set, cl=train_target, k=5)
+classifier_knn <- knn(train=train_set, test=test_set, cl=train_target, k=3)
 m1_table <- table(test_target, classifier_knn)
 misClassError <- mean(classifier_knn != test_target)
 print(paste('Accuracy =', 1-misClassError))
@@ -56,9 +55,8 @@ print(paste('Accuracy =', 1-misClassError))
 # [1] "Accuracy = 0.6"
 # > m1_table
 # classifier_knn
-# test_target 1 2 3
-#           1 3 1 0
-#           2 3 2 0
-#           3 0 0 1
+# test_target 1 2
+#           1 2 3
+#           2 1 4
 
 
