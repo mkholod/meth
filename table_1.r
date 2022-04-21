c934dbab7f36e050eb549d9b87d366322c9065de
Michael Kholodenko <michaelk@ownbackup.com>
2022-04-21 12:03:50 +0300

    #13

--- table_1.r
+++ table_1.r
@@ -6,18 +6,23 @@ install.packages("table1")
 require(table1)
 require(survival)
 
-sample_sheet_filename = "/Sample_Sheet_Full.csv"
+# sample_sheet_filename = "/Sample_Sheet_Full.csv"
+# 
+# path <- paste(baseDir, sample_sheet_filename, sep="")
+# dataOnly = read.csv(path, skip = 8, header = F)
+# headers = read.csv(path, skip = 7, header = F, nrows = 1, as.is = T)
+# 
+# colnames(dataOnly) = headers
+# t1 <- dataOnly
+# t1$Metastasis <- factor(t1$AnyMet, 
+#                                     levels=c('Yes','No'),
+#                                     labels=c('Yes', 'No' # Reference
+#                                              ))
+# 
+# table1(~ Age + Gender + Metastasis | Sample_Group , data=t1)
 
-path <- paste(baseDir, sample_sheet_filename, sep="")
-dataOnly = read.csv(path, skip = 8, header = F)
-headers = read.csv(path, skip = 7, header = F, nrows = 1, as.is = T)
+##### Now with Horvath and Levine ages
 
-colnames(dataOnly) = headers
-t1 <- dataOnly
-t1$Metastasis <- factor(t1$AnyMet, 
-                                    levels=c('Yes','No'),
-                                    labels=c('Yes', 'No' # Reference
-                                             ))
-
-table1(~ Age + Gender + Metastasis | Sample_Group , data=t1)
-
+data_with_clock <- read.csv("csv/myDNAmAge_with_acceleration_age_normalized_with_metadata.csv")
+names(data_with_clock)[names(data_with_clock) == "AnyMet"] <- "Metastasis"
+table1(~ Age + Gender + Metastasis + Horvath + Levine | Sample_Group , data=data_with_clock)
